fd001df55732199676a89307ab42df8353a06235
Ruslan Stosyk <stosh.lv.ua@gmail.com>
2017-05-07 00:04:05 +0300

    add ButterKnife

--- app/src/main/java/com/stosh/fsm/Main.java
+++ app/src/main/java/com/stosh/fsm/Main.java
@@ -3,41 +3,49 @@ package com.stosh.fsm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.stosh.fsm.states.AlarmDisarmed_AllLocked;
 
 import java.util.HashMap;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 /**
  * Created by StoSh on 06-May-17.
  **/
 
-public class Main extends AppCompatActivity implements View.OnClickListener {
+public class Main extends AppCompatActivity{
 
+    private Unbinder unbinder;
     private FSM fsm;
+    @BindView(R.id.statusAlarm)
+    CheckBox statusAlarm;
+    @BindView(R.id.statusDoor)
+    TextView statusDoor;
+    @BindView(R.id.status)
+    LinearLayout status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setListenerForButton();
+        unbinder = ButterKnife.bind(this);
         HashMap<Integer, View> resources = new HashMap<>();
-        resources.put(0, findViewById(R.id.statusAlarm));
-        resources.put(1, findViewById(R.id.statusDoor));
-        resources.put(2, findViewById(R.id.status));
+        resources.put(0, statusAlarm);
+        resources.put(1, statusDoor);
+        resources.put(2, status);
         fsm = new FSM(new AlarmDisarmed_AllLocked(resources));
     }
 
-    private void setListenerForButton(){
-        findViewById(R.id.btnUnlock).setOnClickListener(this);
-        findViewById(R.id.btnUnlockX2).setOnClickListener(this);
-        findViewById(R.id.btnLock).setOnClickListener(this);
-        findViewById(R.id.btnLockX2).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
+    @OnClick({R.id.btnLock, R.id.btnUnlock, R.id.btnLockX2, R.id.btnUnlockX2})
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
             case R.id.btnLock:
                 fsm.getState().onLock(fsm);
                 break;
@@ -52,4 +60,11 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
                 break;
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
 }
+
